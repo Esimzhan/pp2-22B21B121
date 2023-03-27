26f15adfaa884b5737b926d1b1f6155bb50bae6b
Esimzhan <123537999+Esimzhan@users.noreply.github.com>
2023-03-27 21:40:31 +0600

    k

---
+++ pygame/3.py
@@ -0,0 +1,31 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+done = False
+is_blue = True
+x = 30
+y = 30
+
+clock = pygame.time.Clock()
+
+while not done:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_UP]: y -= 3
+    if pressed[pygame.K_DOWN]: y += 3
+    if pressed[pygame.K_LEFT]: x -= 3
+    if pressed[pygame.K_RIGHT]: x += 3
+
+    screen.fill((0, 0, 0))
+    if is_blue:
+        color = (0, 128, 255)
+    else:
+        color = (255, 100, 0)
+    pygame.draw.circle(screen, color, (x, y), 20)
+
+    pygame.display.flip()
+    clock.tick(60)
